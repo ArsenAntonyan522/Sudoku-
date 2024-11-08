46e6255234c673092ca05c48c50a66faf93368bf
ArsenAntonyan522 <arstothen@gmail.com>
2024-11-08 16:56:21 +0300

    final

--- sudoku.js
+++ sudoku.js
@@ -8,6 +8,21 @@ function read() {
 
 }
 
+const example = read()
+
+function solve(example) {
+const arr = example.split('');
+  const board = [];
+  for (let i = 0; i < arr.length; i += 9) {
+    const arrMini = arr.slice(i, i + 9);
+    board.push(arrMini.map((el) => (isNaN(+el) ? 0 : +el)));
+  }
+  return board;
+}
+
+console.table(solve(example))
+
+
 
 function isSolved(numberRand) {
   const existedNumbers = [];
@@ -39,25 +54,5 @@ function isSolved(numberRand) {
 
 isSolved(solve(read()));
 
-function prettyBoard() {
- 
-}
-
-function prettyBoard(board) {
-}
-
-const example = read()
-
-function solve(example) {
-const arr = example.split('');
-  const board = [];
-  for (let i = 0; i < arr.length; i += 9) {
-    const arrMini = arr.slice(i, i + 9);
-    board.push(arrMini.map((el) => (isNaN(+el) ? 0 : +el)));
-  }
-  return board;
-}
-
-console.table(solve(example))
 
 
